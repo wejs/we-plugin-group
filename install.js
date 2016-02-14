f7167e0efbdd5f143aced62f938141e947bb5f3e
albertosouza <alberto.souza.99@gmail.com>
2016-02-14 00:41:19 -0200

    add default role permissions in role creation

--- install.js
+++ install.js
@@ -20,10 +20,45 @@ module.exports = {
       function registerDefaultRoles(done) {
         we.utils.async.parallel([
           function memberRole(done) {
-            we.acl.registerOneDefaltRole(we, 'groupMember', done);
+            // create the administrator role
+            we.db.models.role.findOrCreate({
+              where: { name: 'groupMember' },
+              defaults: {
+                name: 'groupMember',
+                isSystemRole: true,
+                permissions: [
+                  'find_group',
+                  'find_post'
+                ]
+              }
+            }).spread(function (role) {
+              // set the role in global roles how will be avaible in we.acl.roles
+              we.acl.roles.groupMember = role;
+              done(null , role);
+            }).catch(done);
           },
           function managerRole(done) {
-            we.acl.registerOneDefaltRole(we, 'groupManager', done);
+            // create the administrator role
+            we.db.models.role.findOrCreate({
+              where: { name: 'groupManager' },
+              defaults: {
+                name: 'groupManager',
+                isSystemRole: true,
+                permissions: [
+                  'update_group',
+                  'find_group',
+                  'delete_group',
+                  'manage_group',
+                  'find_group_member',
+                  'post_highlight'
+                ]
+              }
+            }).spread(function (role) {
+              // set the role in global roles how will be avaible in we.acl.roles
+              we.acl.roles.groupManager = role;
+              done(null , role);
+            }).catch(done);
+
           }
         ], done);
       },
