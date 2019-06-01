8882b414b8a5c8d2359984278af8394a24f3634c
albertosouza <alberto.souza.99@gmail.com>
2019-05-31 21:58:49 -0300

    change tests to run with sqlite

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,39 +1,57 @@
 const projectPath = process.cwd(),
   testTools = require('we-test-tools'),
+  deleteDir = require('rimraf'),
   path = require('path');
 
 let we;
 
+
+before(function (callback) {
+  testTools.copyLocalSQLiteConfigIfNotExitst(projectPath, callback);
+});
+
 before(function(callback) {
   this.slow(100);
 
-  testTools.copyLocalConfigIfNotExitst(projectPath, function() {
-    const We = require('we-core');
-    we = new We();
+  const We = require('we-core');
+  we = new We();
 
-    testTools.init({}, we);
+  testTools.init({}, we);
 
-    we.bootstrap({
-      i18n: {
-        directory: path.join(__dirname, 'locales'),
-        updateFiles: true
-      }
-    } , function(err, we) {
+  we.bootstrap({
+    i18n: {
+      directory: path.join(__dirname, 'locales'),
+      updateFiles: true
+    }
+  } , function(err, we) {
+    if (err) throw err;
+
+    we.startServer(function(err) {
       if (err) throw err;
-
-      we.startServer(function(err) {
-        if (err) throw err;
-        callback();
-      });
+      callback();
     });
   });
+
 });
 
+// after all tests remove test folders and delete the database:
 after(function (callback) {
-  testTools.helpers.resetDatabase(we, callback);
-});
+  testTools.helpers.resetDatabase(we, (err)=> {
+    if(err) return callback(err);
 
-//after all tests
-after(function () {
-  we.exit(process.exit);
+    const tempFolders = [
+      projectPath + '/files/tmp',
+      projectPath + '/files/config',
+      projectPath + '/files/sqlite',
+      projectPath + '/files/uploads',
+      projectPath + '/files/templatesCacheBuilds.js'
+    ];
+
+    we.utils.async.each(tempFolders, (folder, next)=> {
+      deleteDir( folder, next);
+    }, (err)=> {
+      if (err) throw new Error(err);
+      we.exit(callback);
+    });
+  });
 });
